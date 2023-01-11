9caa4432a88316dd517f9783f94d1a68c057af92
MatthieuALLIER <alrmatth@gmail.com>
2023-01-11 13:11:47 +0100

    changes scrapping

--- loopScraping.py
+++ loopScraping.py
@@ -2,23 +2,21 @@
 """
 Boucle des urls
 """
-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
-import time
+import scrapping, traitement
 
 urls = ["https://www.booking.com/hotel/fr/disney-39-s-new-york-r.fr.html#tab-reviews",
         "https://www.booking.com/hotel/fr/disney-39-s-newport-bay-club-r.fr.html#tab-reviews",
         "https://www.booking.com/hotel/fr/disneys-sequoia-lodge-r.fr.html#tab-reviews",
         "https://www.booking.com/hotel/fr/disney-39-s-cheyenne-r.fr.html#tab-reviews",
         "https://www.booking.com/hotel/fr/disney-39-s-santa-fe-r.fr.html#tab-reviews",
-        "https://www.booking.com/hotel/fr/disneys-davy-crockett-ranch.fr.html#tab-reviews"]
+        "https://www.booking.com/hotel/fr/disneys-davy-crockett-ranch.fr.html#tab-reviews"
+        ]
 DFnames = ["newYork", "newportBay", "sequoiaLodge", "cheyenne", "santaFe", "davyCrockettRanch"]
 
 driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 for num in range(len(urls)):
     url = urls[num]
     nameDF = DFnames[num]
-    exec(open('GIT/scrapping.py').read())
-    exec(open('GIT/traitement.py').read())
+    scrappedData = scrapping.Scrapping(driver, url)
+    globals()[nameDF] = traitement.Traitement(nameDF, scrappedData)
--- scrapping.py
+++ scrapping.py
@@ -2,90 +2,98 @@
 """
 Fichier scraping Booking Disneyland
 """
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
 
-userNames = []
-notes = []
-country = []
-titles = []
-posComs = []
-posNeg = []
-travelDate = []
-comDate = []  
-
-driver.get(url)
-try:
-    driver.find_element(By.ID, "onetrust-reject-all-handler").click()
-except:
-    pass
-
-while(True):    
-    content = driver.page_source
-    soup = BeautifulSoup(content)
-    for a in soup.findAll(attrs={"class":"review_list_new_item_block"}):
-        try:
-            name = a.find("span", attrs={"class":"bui-avatar-block__title"}).text
-        except:
-            name = None
-        try:
-            note = a.find("div", attrs={"class":"bui-review-score__badge"}).text
-        except:
-            note = None
-        try:
-            pays = a.find("span", attrs={"class":"bui-avatar-block__subtitle"}).text
-        except:
-            pays = None
-        try:
-            titre = a.find("h3", attrs={"class":"c-review-block__title c-review__title--ltr"}).text
-        except:
-            titre = None
-        
-        #Commentaires
-        try:
-            com1 = a.find("div", attrs={"class":"c-review__row"})
+def Scrapping(driver, url):
+    userNames = []
+    notes = []
+    country = []
+    titles = []
+    posComs = []
+    posNeg = []
+    travelDate = []
+    comDate = []  
+    
+    driver.get(url)
+    try:
+        driver.find_element(By.ID, "onetrust-reject-all-handler").click()
+    except:
+        pass
+    
+    while(True):    
+        content = driver.page_source
+        soup = BeautifulSoup(content)
+        for a in soup.findAll(attrs={"class":"review_list_new_item_block"}):
             try:
-                compos = com1.find("span", attrs={"class":"c-review__body"}).text
+                name = a.find("span", attrs={"class":"bui-avatar-block__title"}).text
             except:
+                name = None
+            try:
+                note = a.find("div", attrs={"class":"bui-review-score__badge"}).text
+            except:
+                note = None
+            try:
+                pays = a.find("span", attrs={"class":"bui-avatar-block__subtitle"}).text
+            except:
+                pays = None
+            try:
+                titre = a.find("h3", attrs={"class":"c-review-block__title c-review__title--ltr"}).text
+            except:
+                titre = None
+            
+            #Commentaires
+            try:
                 compos = None
-        except:
-            compos = None        
-        try:
-            com2 = a.find("div", attrs={"class":"c-review__row lalala"})
-            try:
-                comneg = com2.find("span", attrs={"class":"c-review__body"}).text
-            except:
                 comneg = None
-        except:            
-            comneg = None
-        #Dates
-        try:
-            leftBloc = a.find("div", attrs={"class":"bui-grid__column-3 c-review-block__left"})
+                for com in a.findAll("div", attrs={"class":"c-review__row"}):
+                    if com.find("span", attrs={"class":"bui-u-sr-only"}).text == "A aimé":
+                        compos = com.find("span", attrs={"class":"c-review__body"}).text
+                    elif com.find("span", attrs={"class":"bui-u-sr-only"}).text == "N'a pas aimé":
+                        comneg = com.find("span", attrs={"class":"c-review__body"}).text
+            except:
+                pass
+            
+            #Dates
             try:
-                date_sejour = leftBloc.find("span", attrs={"class":"c-review-block__date"}).text
+                leftBloc = a.find("div", attrs={"class":"bui-grid__column-3 c-review-block__left"})
+                try:
+                    date_sejour = leftBloc.find("span", attrs={"class":"c-review-block__date"}).text
+                except:
+                    date_sejour = None
             except:
                 date_sejour = None
-        except:
-            date_sejour = None
-        
-        try:
-            rightBloc = a.find("div", attrs={"class":"bui-grid__column-9 c-review-block__right"})
+            
             try:
-                date_commentaire = rightBloc.find("span", attrs={"class":"c-review-block__date"}).text
+                rightBloc = a.find("div", attrs={"class":"bui-grid__column-9 c-review-block__right"})
+                try:
+                    date_commentaire = rightBloc.find("span", attrs={"class":"c-review-block__date"}).text
+                except:
+                    date_commentaire = None
             except:
                 date_commentaire = None
-        except:
-            date_commentaire = None
+                
+            userNames.append(name)
+            notes.append(note)
+            country.append(pays)
+            titles.append(titre)
+            posComs.append(compos)
+            posNeg.append(comneg)
+            travelDate.append(date_sejour)
+            comDate.append(date_commentaire)
             
-        userNames.append(name)
-        notes.append(note)
-        country.append(pays)
-        titles.append(titre)
-        posComs.append(compos)
-        posNeg.append(comneg)
-        travelDate.append(date_sejour)
-        comDate.append(date_commentaire)
+        time.sleep(1)
+        try:
+            driver.find_element(By.CLASS_NAME,  "pagenext").click()
+        except:
+            return {"userNames":userNames, 
+                    "notes":notes,
+                    "country":country,
+                    "titles":titles,
+                    "posComs":posComs,
+                    "posNeg":posNeg,
+                    "travelDate":travelDate,
+                    "comDate":comDate}
         
-    time.sleep(1)
-    try:
-        driver.find_element(By.CLASS_NAME,  "pagenext").click()
-    except:
-        break
+        
