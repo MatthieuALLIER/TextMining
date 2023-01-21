f14fc7ffc89bc960147c2c46e767dd670b8bd9a2
Robin Ponson <rponson2001@gmail.com>
2023-01-21 13:09:15 +0100

    Oui encore

--- app.py
+++ app.py
@@ -15,17 +15,38 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 import plotly.graph_objects as go
 from datetime import date
+from Lib import nettoyage
+from analyse_date import analyseDate
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], title='Disney textmining')
 server = app.server
-
+print("Importation des données")
 disney=pd.read_csv("data/disney.csv")
 
+## Nettoyage
+# Titre
+print("Nettoyage des titres")
+titre=nettoyage.nettoyage_corpus(disney.Titre)
+titre_join = [" ".join(w)for w in titre]
+# Commentairs positifs
+print("Nettoyage des commentaires positifs")
+positif=nettoyage.nettoyage_corpus(disney.Positif)
+positif_join = [" ".join(w)for w in positif]
+# Commentaires négatifs
+print("Nettoyage des commentaires négqtifs")
+negatif=nettoyage.nettoyage_corpus(disney.Négatif)
+negatif_join = [" ".join(w)for w in negatif]
 # Passage de la colonne date_sejour en format date
+print("Formatage des dates")
 import dateparser 
 date_sejour = disney["Date séjour"].tolist()
 date_sejour=[dateparser.parse(date) for date in date_sejour]
+annee=[date.year for date in date_sejour]
 
+# Fonction d'analyse par année
+posdate=analyseDate(positif_join,annee)
+negdate=analyseDate(positif_join,annee)
+titredate=analyseDate(positif_join,annee)
 #Menu
 TABPANEL = dbc.Container([
     html.H2("Analyse des commentaires Disney"),
@@ -72,11 +93,39 @@ PageContent = dbc.Container([
                 ], id="tabPanelAnalyse", active_tab="date"),
         html.Div([
             #Date
-            dcc.DatePickerRange(
-                display_format='M/Y',
-                start_date=min(date_sejour),
-                end_date=max(date_sejour)
-             )
+            # dcc.DatePickerRange(
+            #     display_format='M/Y',
+            #     start_date=min(date_sejour),
+            #     end_date=max(date_sejour)
+            #  )
+            dcc.Dropdown(
+                   options={"1":'Titre', "2":'Commentaires positifs', "3":'Commentaires négatifs'},
+                   value="1",
+                   id="liste_choix_corpus_date"
+             ),
+            html.Div([
+               html.Img(id='image_titre_date_2019'),
+               html.Img(id='image_titre_date_2020'),
+               html.Img(id='image_titre_date_2021'),
+               html.Img(id='image_titre_date_2022'),
+               html.Img(id='image_titre_date_2023')
+            ], id="date-titre", style= {'display': 'none'}),
+            
+            html.Div([
+               html.Img(id='image_positifs_date_2019'),
+               html.Img(id='image_positifs_date_2020'),
+               html.Img(id='image_positifs_date_2021'),
+               html.Img(id='image_positifs_date_2022'),
+               html.Img(id='image_positifs_date_2023')
+            ], id="date-positif", style= {'display': 'none'}),
+            
+            html.Div([
+               html.Img(id='image_negatifs_date_2019'),
+               html.Img(id='image_negatifs_date_2020'),
+               html.Img(id='image_negatifs_date_2021'),
+               html.Img(id='image_negatifs_date_2022'),
+               html.Img(id='image_negatifs_date_2023')
+            ], id="date-negatif", style= {'display': 'none'})
             
         ], id="date-tab", style= {'display': 'none'}),
         html.Div([
@@ -153,7 +202,28 @@ def tabChangeAnalyse(value):
                 {'display': 'none'},
                 {'display': 'none'},
                 {'display': 'block'}]
-
+    
+@app.callback( [Output('date-titre', 'style'),
+               Output('date-positif', 'style'),
+               Output('date-negatif', 'style')
+               ],
+               [Input('liste_choix_corpus_date', 'value')])
+def DateChangeCorpus(value):
+    print(value)
+    if value == "1":
+        return [{'display': 'block'},
+                {'display': 'none'},
+                {'display': 'none'}]
+    
+    if value == "2":
+        return [{'display': 'none'},
+                {'display': 'block'},
+                {'display': 'none'}]
+    
+    if value == "3":
+        return [{'display': 'none'},
+                {'display': 'none'},
+                {'display': 'block'}]
 #Lauch
 if __name__ == '__main__':
     app.run_server(debug=True, use_reloader=False)
