944f6dc80af95d198bd2d2d6bd791c14b80e5e6e
AxelEutarici <114355274+AxelEutarici@users.noreply.github.com>
2023-01-21 18:00:12 +0100

    Ajout d'une map avec nb commentaires/pays
    C'est un gros block, j'ai pas trouvé de package qui faisait de traduction potable

--- fonctions_graphiques.py
+++ fonctions_graphiques.py
@@ -14,12 +14,74 @@ from wordcloud import WordCloud
 #ajoute une colonne nbc avec des 1 et supprime 8 lignes sans le pays
 df_sunb = disney.drop(disney.index[disney.iloc[:,2].isnull()],0).assign(nbc = 1)
 
+df_pays_nb = disney['Pays'].value_counts().to_frame().reset_index().rename(columns={'index': 'Pays', 'Pays': 'occ'})
+print(df_pays_nb)
+
+
+# Pour le df de la carte du monde, déso j'ai pas trouvé plus court, les packages de traductions fonctionnaient pas très bien 
+
+old_values = ['Finlande', 'La Réunion', 'France', 'Espagne', 'Belgique',
+       'Suisse', 'Maroc', 'Guadeloupe', 'États-Unis', 'Allemagne',
+       'Tunisie', 'Japon', 'Polynésie française', 'Nouvelle-Calédonie',
+       'Brésil', 'Canada', 'Pays-Bas', 'Royaume-Uni', 'Irlande',
+       'Singapour', 'Arabie saoudite', 'Israël', 'Australie', 'Malaisie',
+       'Portugal', 'Norvège', 'Bulgarie', 'Thaïlande', 'Grèce',
+       'République tchèque', 'Argentine', 'Luxembourg', 'Malte',
+       'Mexique', 'Brunei', 'Hongrie', 'Géorgie', 'Émirats arabes unis',
+       'Roumanie', 'Estonie', 'Gibraltar', 'Pologne', 'Turquie', 'Égypte',
+       'Indonésie', 'Italie', 'Koweït', 'Autriche', 'Chine', 'Colombie',
+       'Honduras', 'Costa Rica', 'Ukraine', 'Russie', 'Danemark',
+       'Lettonie', 'Panama', 'Pérou', 'Qatar', 'Hong Kong', 'Martinique',
+       'Algérie', 'Monaco', "Côte-d'Ivoire", 'Afrique du Sud', 'Chypre',
+       'Suède', 'Jersey', 'Nouvelle-Zélande', 'Azerbaïdjan', 'Namibie',
+       'Philippines', 'Corée du Sud',
+       'Slovaquie', 'Venezuela', 'Guyane française', 'Croatie',
+       'Chili', 'Taïwan', 'Bénin', 'Madagascar', 'Albanie', 'Bahreïn',
+       'Sri Lanka', 'Liban', 'Oman', 'Mozambique', 'Kazakhstan',
+       'Mayotte', 'Nigeria', 'Jordanie', 'Lituanie', 'Serbie', 'Iran',
+       'Angola', 'Île Maurice', 'Pakistan', 'Cameroun',
+       'Saint-Martin (Antilles françaises)', 'Porto Rico',
+       'Bosnie-Herzégovine', 'Kosovo', 'Laos', 'Irak', 'Inde', 'Slovénie',
+       'Saint-Barthélemy', 'Djibouti', 'Moldavie', 'Andorre', 'Aruba',
+       'Crimée', 'Uruguay', 'Islande']
+
+new_values = ['Finland', 'Reunion', 'France', 'Spain', 'Belgium',
+       'Switzerland', 'Morocco', 'Guadeloupe', 'United States', 'Germany',
+       'Tunisia', 'Japan', 'French Polynesia', 'New Caledonia',
+       'Brazil', 'Canada', 'Netherlands', 'United Kingdom', 'Ireland',
+       'Singapore', 'Saudi Arabia', 'Israel', 'Australia', 'Malaysia',
+       'Portugal', 'Norway', 'Bulgaria', 'Thailand', 'Greece',
+       'Czech Republic', 'Argentina', 'Luxembourg', 'Malta',
+       'Mexico', 'Brunei', 'Hungary', 'Georgia', 'United Arab Emirates',
+       'Romania', 'Estonia', 'Gibraltar', 'Poland', 'Turkey', 'Egypt',
+       'Indonesia', 'Italy', 'Kuwait', 'Austria', 'China', 'Colombia',
+       'Honduras', 'Costa Rica', 'Ukraine', 'Russia', 'Denmark',
+       'Latvia', 'Panama', 'Peru', 'Qatar', 'Hong Kong', 'Martinique',
+       'Algeria', 'Monaco', 'Ivory Coast', 'South Africa', 'Cyprus',
+       'Sweden', 'Jersey', 'New Zealand', 'Azerbaijan', 'Namibia',
+       'Philippines', 'South Korea',
+       'Slovakia', 'Venezuela', 'French Guiana', 'Croatia',
+       'Chile', 'Taiwan', 'Benin', 'Madagascar', 'Albania', 'Bahrain',
+       'Sri Lanka', 'Lebanon', 'Oman', 'Mozambique', 'Kazakhstan',
+       'Mayotte', 'Nigeria', 'Jordan', 'Lithuania', 'Serbia', 'Iran',
+       'Angola', 'Mauritius', 'Pakistan', 'Cameroon',
+       'Saint-Martin (French West Indies)', 'Puerto Rico',
+       'Bosnia and Herzegovina', 'Kosovo', 'Laos', 'Iraq', 'India', 'Slovenia',
+       'Saint-Barthélemy', 'Djibouti', 'Moldova', 'Andorra', 'Aruba',
+       'Crimea', 'Uruguay', 'Iceland']
+
+translation_dict = dict(zip(old_values, new_values))
+
+df_pays_nb['Pays'].replace(translation_dict, inplace=True)
+
+
 
 class Fig :
 
-    def __init__(self, df,df_sunb):
+    def __init__(self, df,df_sunb, df_pays_nb):
         self.__df = df
-        self.__df_sunb = df_sunb 
+        self.__df_sunb = df_sunb
+        self.__df_pays_nb = df_pays_nb
         
 #Sunburst Hotel > Pays > Note
     def get_fig_sunburst(self):
@@ -28,5 +90,14 @@ class Fig :
 #Un graphique à courbes pour montrer l'évolution des notes au fil des mois.
     
 #en cours    
- 
+
+#Carte du monde avec nb de commentaire par pays
+
+    def get_fig_map(self)
+        return fig = px.choropleth(data_frame=sel.__df_pays_nb,
+                    locations='Pays',  # colonne contenant les noms des pays
+                    locationmode='country names',
+                    color='occ',  # colonne contenant le nombre d'occurrences
+                    hover_name='Pays',
+                    title='Nombre d\'occurrences par pays')
 
