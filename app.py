10d51f494ae45b6b77b589726213c24bb5ed418e
MatthieuALLIER <alrmatth@gmail.com>
2023-02-02 12:53:30 +0100

    graphs
    Signed-off-by: MatthieuALLIER <alrmatth@gmail.com>

--- app.py
+++ app.py
@@ -60,6 +60,100 @@ disney["hotel"] = disney["hotel"].replace(list(hotel.id_hotel), list(hotel.hotel
 disney["Pays"] = disney["Pays"].replace(list(pays.id_pays), list(pays.pays))
 disney.Note = pd.to_numeric(disney.Note)        
 
+#ajoute une colonne nbc avec des 1 et supprime 8 lignes sans le pays
+df_sunb = disney.drop(disney.index[disney.iloc[:,2].isnull()],0).assign(nbc = 1)
+
+###########--------------------- DF pour plot comm/mois  ------------------####################
+
+df_commdate = disney
+
+#Séparation mois/annee
+df_commdate["Date séjour"] = df_commdate["Date séjour"].apply(lambda x: x.split(" "))
+df_commdate["mois"] = [i[0] for i in df_commdate["Date séjour"]]
+df_commdate["annee"] = [i[1] for i in df_commdate["Date séjour"]]
+
+# Changement mois en numérique pour traquer l'ordre
+old = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre','Décembre']
+new = ['01','02','03','04','05','06','07','08','09','10','11','12']
+dictio = dict(zip(old, new))
+df_commdate['mois'].replace(dictio, inplace=True)
+
+#Moyenne de note par mois
+agg_df = df_commdate.groupby(["annee","mois"])["Note"].mean().to_frame().reset_index()
+
+#Nouveau index date
+agg_df['date'] = agg_df['annee'].str.cat(agg_df['mois'], sep=' ')
+agg_df = agg_df.set_index('date').drop(columns=["annee", "mois"])
+
+
+
+###########--------------------- DF pour la world map  ------------------####################
+
+#Création df avec Pays et nb commentaires par Pays puis traduction en anglais pour faire fonctionner le graph
+# Pour le df de la carte du monde, déso j'ai pas trouvé plus court, les packages de traductions fonctionnaient pas très bien 
+
+df_pays_nb = disney['Pays'].value_counts().to_frame().reset_index().rename(columns={'index': 'Pays', 'Pays': 'occ'})
+print(df_pays_nb)
+
+old_values = ['Finlande', 'La Réunion', 'France', 'Espagne', 'Belgique',
+       'Suisse', 'Maroc', 'Guadeloupe', 'États-Unis', 'Allemagne',
+       'Tunisie', 'Japon', 'Polynésie française', 'Nouvelle-Calédonie',
+       'Brésil', 'Canada', 'Pays-Bas', 'Royaume-Uni', 'Irlande',
+       'Singapour', 'Arabie saoudite', 'Israël', 'Australie', 'Malaisie',
+       'Portugal', 'Norvège', 'Bulgarie', 'Thaïlande', 'Grèce',
+       'République tchèque', 'Argentine', 'Luxembourg', 'Malte',
+       'Mexique', 'Brunei', 'Hongrie', 'Géorgie', 'Émirats arabes unis',
+       'Roumanie', 'Estonie', 'Gibraltar', 'Pologne', 'Turquie', 'Égypte',
+       'Indonésie', 'Italie', 'Koweït', 'Autriche', 'Chine', 'Colombie',
+       'Honduras', 'Costa Rica', 'Ukraine', 'Russie', 'Danemark',
+       'Lettonie', 'Panama', 'Pérou', 'Qatar', 'Hong Kong', 'Martinique',
+       'Algérie', 'Monaco', "Côte-d'Ivoire", 'Afrique du Sud', 'Chypre',
+       'Suède', 'Jersey', 'Nouvelle-Zélande', 'Azerbaïdjan', 'Namibie',
+       'Philippines', 'Corée du Sud',
+       'Slovaquie', 'Venezuela', 'Guyane française', 'Croatie',
+       'Chili', 'Taïwan', 'Bénin', 'Madagascar', 'Albanie', 'Bahreïn',
+       'Sri Lanka', 'Liban', 'Oman', 'Mozambique', 'Kazakhstan',
+       'Mayotte', 'Nigeria', 'Jordanie', 'Lituanie', 'Serbie', 'Iran',
+       'Angola', 'Île Maurice', 'Pakistan', 'Cameroun',
+       'Saint-Martin (Antilles françaises)', 'Porto Rico',
+       'Bosnie-Herzégovine', 'Kosovo', 'Laos', 'Irak', 'Inde', 'Slovénie',
+       'Saint-Barthélemy', 'Djibouti', 'Moldavie', 'Andorre', 'Aruba',
+       'Crimée', 'Uruguay', 'Islande']
+
+new_values = ['Finland', 'Reunion', 'France', 'Spain', 'Belgium',
+       'Switzerland', 'Morocco', 'Guadeloupe', 'United States', 'Germany',
+       'Tunisia', 'Japan', 'French Polynesia', 'New Caledonia',
+       'Brazil', 'Canada', 'Netherlands', 'United Kingdom', 'Ireland',
+       'Singapore', 'Saudi Arabia', 'Israel', 'Australia', 'Malaysia',
+       'Portugal', 'Norway', 'Bulgaria', 'Thailand', 'Greece',
+       'Czech Republic', 'Argentina', 'Luxembourg', 'Malta',
+       'Mexico', 'Brunei', 'Hungary', 'Georgia', 'United Arab Emirates',
+       'Romania', 'Estonia', 'Gibraltar', 'Poland', 'Turkey', 'Egypt',
+       'Indonesia', 'Italy', 'Kuwait', 'Austria', 'China', 'Colombia',
+       'Honduras', 'Costa Rica', 'Ukraine', 'Russia', 'Denmark',
+       'Latvia', 'Panama', 'Peru', 'Qatar', 'Hong Kong', 'Martinique',
+       'Algeria', 'Monaco', 'Ivory Coast', 'South Africa', 'Cyprus',
+       'Sweden', 'Jersey', 'New Zealand', 'Azerbaijan', 'Namibia',
+       'Philippines', 'South Korea',
+       'Slovakia', 'Venezuela', 'French Guiana', 'Croatia',
+       'Chile', 'Taiwan', 'Benin', 'Madagascar', 'Albania', 'Bahrain',
+       'Sri Lanka', 'Lebanon', 'Oman', 'Mozambique', 'Kazakhstan',
+       'Mayotte', 'Nigeria', 'Jordan', 'Lithuania', 'Serbia', 'Iran',
+       'Angola', 'Mauritius', 'Pakistan', 'Cameroon',
+       'Saint-Martin (French West Indies)', 'Puerto Rico',
+       'Bosnia and Herzegovina', 'Kosovo', 'Laos', 'Iraq', 'India', 'Slovenia',
+       'Saint-Barthélemy', 'Djibouti', 'Moldova', 'Andorra', 'Aruba',
+       'Crimea', 'Uruguay', 'Iceland']
+
+translation_dict = dict(zip(old_values, new_values))
+
+df_pays_nb['Pays'].replace(translation_dict, inplace=True)
+
+from fonctions_graphiques import Fig
+graphs = Fig(disney,df_sunb,df_pays_nb,agg_df)
+sunburst = graphs.get_fig_sunburst()
+carte = graphs.get_fig_map()
+
 f = open('data/datas.json')
 datas = json.load(f)
   
@@ -179,7 +273,7 @@ def tabChangeData(value):
 @app.callback([Output("kpi-tab", "children")],
               [Input("datas", "data")])
 
-def constructionDataeTab(data):
+def constructionDataTab(data):
     global disney
     nbAvis = data["nbAvis"]
     nbGood = data["nbGood"]
@@ -192,39 +286,45 @@ def constructionDataeTab(data):
     noteAvg = data["noteAvg"]
     noteMax = data["noteMax"]
     
-    children = html.Div([], style = {"display":"flex"})
-    children.children.append(
+    children = []
+    children.append(
         html.Div([
-            html.P("Nombre d'avis : "+str(nbAvis)),
-            html.P("Nombre d'avis positifs : "+str(nbGood)),
-            html.P("Nombre d'avis négatifs : "+str(nbBad)),
-            html.P("Date minimum : "+str(dateMin)),
-            html.P("Date maximum : "+str(dateMax)),
-            html.P("Nombre de pays : "+str(nbPays)),
-            html.P("Nombre de pays avec plus de 200 avis : "+str(nbPays200)),
-            html.P("Notes : "),
-            html.P("Minimum : "+str(noteMin)),
-            html.P("Moyenne : "+str(noteAvg)),
-            html.P("Maximum : "+str(noteMax)),
-        ], style = {"width":"25%"})
+            html.Div([
+                html.P("Nombre d'avis : "+str(nbAvis)),
+                html.P("Nombre d'avis positifs : "+str(nbGood)),
+                html.P("Nombre d'avis négatifs : "+str(nbBad)),
+                html.P("Date minimum : "+str(dateMin)),
+                html.P("Date maximum : "+str(dateMax)),
+                html.P("Nombre de pays : "+str(nbPays)),
+                html.P("Nombre de pays avec plus de 200 avis : "+str(nbPays200)),
+                html.P("Notes : "),
+                html.P("Minimum : "+str(noteMin)),
+                html.P("Moyenne : "+str(noteAvg)),
+                html.P("Maximum : "+str(noteMax)),
+            ], style = {"width":"25%"}),
+            html.Div([
+                dash_table.DataTable(
+                    data=disney.to_dict('records'),
+                    columns=[{"name": i, "id": i} for i in disney.columns],
+                    style_data={
+                        'whiteSpace': 'normal',
+                        'height': 'auto',
+                    },
+                    style_table={
+                        'height': 500,
+                        'overflowY': 'scroll',
+                        'width': "100%"
+                    },
+                    sort_action="native"
+                )            
+            ], style = {"width":"75%"})
+        ], style = {"display":"flex"})
     )
-    children.children.append(
+    children.append(
         html.Div([
-            dash_table.DataTable(
-                data=disney.to_dict('records'),
-                columns=[{"name": i, "id": i} for i in disney.columns],
-                style_data={
-                    'whiteSpace': 'normal',
-                    'height': 'auto',
-                },
-                style_table={
-                    'height': 500,
-                    'overflowY': 'scroll',
-                    'width': "100%"
-                },
-                sort_action="native"
-            )            
-        ], style = {"width":"75%"})
+            dcc.Graph(figure = sunburst, style={"width":"80%", "margin":"10%"}),
+            dcc.Graph(figure = carte, style={"width":"80%", "margin":"10%"})
+        ])    
     )
     return [children]
 
